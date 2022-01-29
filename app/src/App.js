fe04bab089147260439dc3c5204659a2cde898fe
spacecardz <98503016+spacecardz@users.noreply.github.com>
2022-01-28 19:10:52 -0800

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -29,3 +29,27 @@ const App = () => {
 };
 
 export default App;
+
+const checkIfWalletIsConnected = async () => {
+  try {
+    const { solana } = window;
+
+    if (solana) {
+      if (solana.isPhantom) {
+        console.log('Phantom wallet found!');
+      }
+    } else {
+      alert('Solana object not found! Get a Phantom Wallet 👻');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+useEffect(() => {
+  const onLoad = async () => {
+    await checkIfWalletIsConnected();
+  };
+  window.addEventListener('load', onLoad);
+  return () => window.removeEventListener('load', onLoad);
+}, []);
