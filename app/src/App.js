a2a8076df8a8881352df2543daa8a3160fd0dab1
spacecardz <98503016+spacecardz@users.noreply.github.com>
2022-01-28 19:12:24 -0800

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -29,3 +29,53 @@ const App = () => {
 };
 
 export default App;
+
+const checkIfWalletIsConnected = async () => {
+  try {
+    const { solana } = window;
+
+    if (solana) {
+      if (solana.isPhantom) {
+        console.log('Phantom wallet found!');
+      }
+    } else {
+      alert('Solana object not found! Get a Phantom Wallet 👻');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+useEffect(() => {
+  const onLoad = async () => {
+    await checkIfWalletIsConnected();
+  };
+  window.addEventListener('load', onLoad);
+  return () => window.removeEventListener('load', onLoad);
+}, []);
+
+const checkIfWalletIsConnected = async () => {
+  try {
+    const { solana } = window;
+
+    if (solana) {
+      if (solana.isPhantom) {
+        console.log('Phantom wallet found!');
+
+        /*
+         * The solana object gives us a function that will allow us to connect
+         * directly with the user's wallet!
+         */
+        const response = await solana.connect({ onlyIfTrusted: true });
+        console.log(
+          'Connected with Public Key:',
+          response.publicKey.toString()
+        );
+      }
+    } else {
+      alert('Solana object not found! Get a Phantom Wallet 👻');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
